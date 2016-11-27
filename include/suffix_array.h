f2922ecb37ca8bc655cc2f71c2c266f5ac9c96e0
ViniciusCPSouza <vcps@cin.ufpe.br>
2016-11-27 00:25:05 -0300

    Building the suffix array using a sort index

--- include/suffix_array.h
+++ include/suffix_array.h
@@ -1,10 +1,12 @@
 #ifndef SUFFIX_ARRAY_H
 #define SUFFIX_ARRAY_H
 
+#include <map>
 #include <string>
 #include <vector>
 
-namespace suffix_array {
+namespace suffix_array
+{
 	std::vector<int> getSuffixArray(std::string text);
 }
 
--- src/suffix_array.cpp
+++ src/suffix_array.cpp
@@ -1,80 +1,70 @@
 #include <suffix_array.h>
 
 // TODO: REMOVE THIIIIIIIIIIIIIIIIIS
-#include <iostream>
+#include <algorithm>
 
-#define MAX_CHAR 255
 
-namespace suffix_array {
-	std::vector<int> radixSort(std::string original, std::vector<int> sa, int max_value, int offset) {
-		std::vector<int> output(sa.size(), 0);
-		int count[max_value + 1];
+namespace suffix_array
+{
+	struct step_entry
+	{
+		int tuple[2];
+		int original_index;
+	};
 
-		// set all counters to 0
-		for (int i = 0; i <= max_value; i++) count[i] = 0;
-		// count each occurrence in the origin string, using the current offset
-		for (std::vector<int>::iterator it = sa.begin(); it != sa.end(); it++)
-		{
-			count[original[(*it) + offset]]++;
-		}
-		// exclusive prefix sum
-		for (int i = 0, sum = 0; i <= max_value; i++){
-			int old_count = count[i];
-			count[i] = sum;
-			sum += old_count;
-		}
-		// actually sort
-		for (std::vector<int>::iterator it = sa.begin(); it != sa.end(); it++)
-		{
-			output[count[original[*it + offset]]++] = *it;
-		}
-
-		return output;
+	int cmp(struct step_entry a, struct step_entry b)
+	{
+	  return (a.tuple[0] == b.tuple[0]) ? (a.tuple[1] < b.tuple[1] ? 1: 0) : (a.tuple[0] < b.tuple[0] ? 1: 0);
 	}
 
 	std::vector<int> getSuffixArray(std::string text)
 	{
-		std::vector<int> sa;
+		int N = text.length();
+		std::vector<int> SA(N, 0);
+		std::map<int, std::vector<int>> sort_index;
+		step_entry tuple_array [N];
 
-		if (text.length() == 0) return sa;
-
-		// [1] Construct the suffix array A12 of the suffixes starting at positions i diff 0 (mod 3).
-		// This is done by a recursive call of the skew algorithm for a string of two thirds the length.
-
-		//// [1.1] Lexicographically name all triples T[i..i + 2]
-		text = text.append(3, (char)0);
-
-		std::vector<int> lex_names;
-		for (int i = 1; i < text.length() - 3; i++)
+		// initialize the sort index
+		sort_index[0] = std::vector<int>(N, 0);
+		for (int i = 0; i < N; i++)
 		{
-			if ((i % 3) != 0)
+			sort_index[0][i] = text[i] - 'a';
+		}
+
+		int step, count;
+		for(step=1, count=1; count < N; step++, count *= 2)
+		{
+			// current iteration of the tuple list
+			for (int i = 0; i < N; i++)
 			{
-				lex_names.push_back(i);
+        tuple_array[i].tuple[0] = sort_index[step - 1][i];
+        tuple_array[i].tuple[1] = (i + count) < N ? sort_index[step - 1][i + count] : -1;
+        tuple_array[i].original_index = i;
+			}
+
+			// sort the new tuples
+			std::sort(tuple_array, tuple_array + N, cmp);
+
+			sort_index[step] = std::vector<int>(N, 0);
+			for (int i = 0; i < N; i++)
+			{
+				if ((i > 0) && (tuple_array[i].tuple[0] == tuple_array[i - 1].tuple[0]) &&
+					  (tuple_array[i].tuple[1] == tuple_array[i - 1].tuple[1]))
+				{
+					sort_index[step][tuple_array[i].original_index] = sort_index[step][tuple_array[i - 1].original_index];
+				}
+				else
+				{
+					sort_index[step][tuple_array[i].original_index] = i;
+				}
 			}
 		}
 
-		// three passes of radix sort, for each character of the triple
-		lex_names = radixSort(text, lex_names, MAX_CHAR, 2);
-		lex_names = radixSort(text, lex_names, MAX_CHAR, 1);
-		lex_names = radixSort(text, lex_names, MAX_CHAR, 0);
-
-		for (std::vector<int>::iterator it = lex_names.begin(); it != lex_names.end(); it++)
+		for (int i = 0; i < N; i++)
 		{
-			std::cout << *it << "," << std::endl;
+			SA[i] = tuple_array[i].original_index;
 		}
 
-		//// [1.2] Construct a text T' of triple names
-
-		//// [1.3] Construct suffix array A' of T' (recursively)
-
-		//// [1.4] Transform A' into A12
-
-		// [2] Construct the suffix array A0 of the remaining suffixes using the result of the first step.
-		// ...
-		
-		// [3] Merge the two suffix arrays into one.
-		// ...
-
-		return sa;
+		return SA;
 	}
 }
