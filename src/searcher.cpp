25e2d367a3079dda7ec55458c894a845517faee3
ViniciusCPSouza <vcps@cin.ufpe.br>
2016-11-27 16:52:43 -0300

    Working search using the suffix array

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -5,9 +5,9 @@ namespace searcher
 {
 	enum LineType {SUFFIX_ARRAY, TEXT_LINE};
 
-	std::map<int, std::vector<int>> search_index(std::string index_file, std::vector<std::string> patterns)
+	std::map<int, LineResult> search_index(std::string index_file, std::vector<std::string> patterns)
 	{
-		std::map<int, std::vector<int>> occurrences;
+		std::map<int, LineResult> occurrences;
 
 		std::ifstream input(index_file);
 		std::string line;
@@ -27,8 +27,15 @@ namespace searcher
 				current_line++;
 				lineType = SUFFIX_ARRAY;
 
-				// search in line for the occurrences
-				occurrences[current_line] = search(line, sa);
+				occurrences[current_line] = LineResult();
+				occurrences[current_line].occurrences = std::vector<int>();
+				std::vector<int> results;
+				for (std::vector<std::string>::iterator it = patterns.begin(); it != patterns.end(); it++)
+				{
+					results = search(line, sa, *it);
+					occurrences[current_line].occurrences.insert(occurrences[current_line].occurrences.end(), results.begin(), results.end());
+				}
+				if (occurrences[current_line].occurrences.size() > 0) occurrences[current_line].line = line;
 			}
 		}
 
@@ -48,13 +55,92 @@ namespace searcher
 		return sa;
 	}
 
-	std::vector<int> search(std::string text, std::vector<int> sa)
+	std::vector<int> search(std::string text, std::vector<int> sa, std::string pattern)
 	{
 		std::vector<int> occurrences;
+		int N = pattern.length();
+		int T = text.length();
 
-		// TODO: search
-		// ...
+		if (N == 0 || T == 0 || (compare(pattern, text, sa[0]) < 0) || (compare(pattern, text, sa[T-1]) > 1)) return occurrences;
+
+		int L = 0, R = T - 1, M = 0, comp;
+		bool found = false;
+		while(R - L > 1)
+		{
+			M = (R + L) / 2;
+			comp = compare(pattern, text, sa[M]);
+
+			if (comp < 0)
+			{
+				R = M;
+			}
+			else if (comp > 0)
+			{
+				L = M;
+			}
+			else
+			{
+				L = R = M;
+				found = true;
+			}
+		}
+
+		// occ!
+		if (found)
+		{
+			// find the neighbours
+			occurrences.push_back(sa[M]);
+
+			for (int nl = M - 1, nr = M + 1; nl >= 0 && nr < T; nl--, nr++)
+			{
+				// occ!
+				if (compare(pattern, text, sa[nl]) == 0)
+				{
+					occurrences.insert(occurrences.begin(), sa[nl]);
+				}
+				else
+				{
+					nl = -1;
+				}
+
+				// occ!
+				if (compare(pattern, text, sa[nr]) == 0)
+				{
+					occurrences.push_back(sa[nr]);
+				}
+				else
+				{
+					nr = T;	
+				}
+			}
+		}
 
 		return occurrences;
+	}
+
+	int compare(std::string pattern, std::string text, int offset)
+	{
+		// if the patter is bigger than the prefix, we can only be lesser than
+		// or grater than, but never be included in the prefix,
+		// otherwise, we start with the supposition that we are included
+		// in the suffix
+		int status = (pattern.length() <= (text.length() - offset)) ? 0 : 1;
+
+		for(int p = 0, t = offset; p < pattern.length() && t < text.length(); p++, t++)
+		{
+			if (pattern[p] < text[t])
+			{
+				status = -1;
+				break;
+			}
+
+			if (pattern[p] > text[t])
+			{
+				status = 1;
+				break;	
+			}
+		}
+
+		return status;
 	}
 }
